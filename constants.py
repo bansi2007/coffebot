24a80d2000db6af31e5b469665f08090ea64b6a4
Bansi Manvar <65157355+bansi2007@users.noreply.github.com>
2021-04-09 21:45:16 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,3 @@
+BOT_NAME = "\\put our slash command name"
+##here you have to write the channels which are to be restricted to use this slash command.
+supported_channels = ["channel_name1","channel_name2"]
